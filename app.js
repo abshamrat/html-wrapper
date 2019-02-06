02a42f0f6391272e13764d98a109b18d349caedb
shamrat17 <shamratakbar17@gmail.com>
2019-02-06 10:24:40 +0600

    wrapped file saving

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const wrapper = require('./src/wrapper');
 const path = require('path');
+const pattern = `<code class=${process.env.ELEM_CLASS}> <%= html %> </code>`;
+const lookElem = process.env.LOOK_ELEM;
 
+console.log(process.env.STRICT_WRAP === 'false');
 
-console.log(wrapper.wrapFilesBy('asdfasd'));
+console.log(wrapper.wrapFilesBy(pattern, lookElem));
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,6 +1,7 @@
 const env = require('dotenv-loader');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const _ = require('lodash');
 var isValid = require('is-valid-path');
 
 
@@ -19,15 +20,38 @@ async function getFileList (srcDir) {
     return false;
 }
 
+async function saveChange(fileName, content) {
+    try {
+        fs.writeFileSync(`${process.env.SRC_DIR+fileName}`, content, process.ENCODING);
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
     getFileList: getFileList,
-    wrapFilesBy: async (pattern) => {
+    wrapFilesBy: async (pattern, lookElem) => {
         const fileList = await getFileList(process.env.SRC_DIR);
-        fileList.map((fileName) => {
+        fileList.map(async (fileName) => {
             const html  = fs.readFileSync(process.env.SRC_DIR+fileName, process.env.ENCODING);
-            const $ = cheerio.load(html);
-            console.log($('pre').html());
-
+            const $ = await cheerio.load(html);
+            if ($(lookElem).find('code').length < 1 || process.env.STRICT_WRAP === 'true') {
+                const compiled = await _.template(pattern);
+                $(lookElem).html(await compiled({html: `${$(lookElem).html()}`}));
+                await saveChange(fileName, $.html());
+            }
+        });
+    },
+    appendElements: async (elements) => {
+        const fileList = await getFileList(process.env.SRC_DIR);
+        fileList.map(async (fileName) => {
+            const html  = fs.readFileSync(process.env.SRC_DIR+fileName, process.env.ENCODING);
+            const $ = await cheerio.load(html);
+            if ($(lookElem).find('code').length < 1 || process.env.STRICT_WRAP === 'true') {
+                const compiled = await _.template(pattern);
+                $(lookElem).html(await compiled({html: `${$(lookElem).html()}`}));
+                await saveChange(fileName, $.html());
+            }
         });
     }
 
